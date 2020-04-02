28db71400067c2ed6524eced2d56ee8ce9e997e4
yuichiyasui <yassan.b.a.w@gmail.com>
2020-04-03 03:15:01 +0900

    ナビゲーションバーをスマホサイズに対応

---
+++ src/components/Chart.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-col class="text-center">
+      <div id="chart_div"></div>
+      <div id="read_text"></div>
+    </v-col>
+</template>
+
+<script>
+  export default {
+    name: 'Chart',
+    data: () => ({})
+  }
+</script>
