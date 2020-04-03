cdaf88bb937e6661ed9452faa77ab9ab86da8df7
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2020-04-03 16:06:26 +0900

    感染者数推移コンポーネント作成

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -6,16 +6,24 @@
         <Chart />
       </v-card-text>
     </v-card>
+    <v-card height="80vh" class="pa-3">
+      <v-card-text>
+        <p class="display-1">感染者推移グラフを表示します</p>
+        <TransitionGraph />
+      </v-card-text>
+    </v-card>
   </v-col>
 </template>
 
 <script>
 import Chart from "@/components/Chart.vue";
+import TransitionGraph from "@/components/TransitionGraph.vue";
 
 export default {
   name: "Home",
   components: {
-    Chart
+    Chart,
+    TransitionGraph
   }
 };
 </script>
