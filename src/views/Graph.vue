39f1e7d2398903ac351ed08226ac08b643d8a8bc
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2020-04-06 10:01:30 +0900

    テスト

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -2,12 +2,6 @@
   <v-col>
     <v-card height="80vh" class="pa-3">
       <v-card-text>
-        <p class="display-1">グラフを表示します</p>
-         <Chart />
-      </v-card-text>
-    </v-card>
-    <v-card height="80vh" class="pa-3">
-      <v-card-text>
         <p class="display-1">感染者推移グラフを表示します</p>
         <TransitionGraph />
       </v-card-text>
@@ -16,13 +10,11 @@
 </template>
 
 <script>
-import Chart from "@/components/Chart.vue";
 import TransitionGraph from "@/components/TransitionGraph.vue";
 
 export default {
   name: "Home",
   components: {
-    Chart,
     TransitionGraph
   }
 };
