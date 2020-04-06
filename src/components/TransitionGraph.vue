6598c1fce18ccb2dcdbb3109b2e33a7b4c1b93c1
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2020-04-06 16:52:42 +0900

    グラフの作成

--- src/components/TransitionGraph.vue
+++ src/components/TransitionGraph.vue
@@ -1,17 +1,30 @@
 <template>
-  <div>
-    <h1>graph</h1>
-    <p>{{ masterData }}</p>
+  <div class="small">
+    <line-chart :chart-data="infectedPerson"></line-chart>
+    <button @click="fillData()">Randomize</button>
   </div>
 </template>
-
 <script>
+  import LineChart from '../LineChart.js'
   export default {
-    name: 'TransitionGraph',
-    data(){
-      return{
-      masterData: this.$store.state.masterData
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        infectedPerson: {
+          labels: ["2020/04/01", "2020/04/01", "2020/04/01", "2020/04/01"],
+          datasets: [
+            {
+              label: '感染者数',        
+              data: [2, 20, 200, 60]
+            }, 
+          ]
+        },
+        date: this.$store.state.masterData
       }
-    }
+    },
+    mounted () {},
+    methods: {}
   }
 </script>
