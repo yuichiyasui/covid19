f22ab2b7a27db204bb59fcade00747b53bb0fd3c
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2020-04-06 13:48:51 +0900

    データのバインディングテスト

--- src/components/TransitionGraph.vue
+++ src/components/TransitionGraph.vue
@@ -1,10 +1,17 @@
 <template>
+  <div>
     <h1>graph</h1>
+    <p>{{ masterData }}</p>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'TransitionGraph',
-    data: () => ({})
+    data(){
+      return{
+      masterData: this.$store.state.masterData
+      }
+    }
   }
 </script>
--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -6,6 +6,7 @@
         <Chart />
       </v-card-text>
     </v-card>
+    
     <v-card height="80vh" class="pa-3">
       <v-card-text>
         <p class="display-1">感染者推移グラフを表示します</p>
