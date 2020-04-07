0ce848ef6a3308c0a5d0a3bf67c3cb0623eb319d
ikedariho <riho.ikeda@rakus-partners.co.jp>
2020-04-07 11:35:50 +0900

    性別、年代別感染者数テンプレート完成

---
+++ src/components/AgeGraph.vue
@@ -0,0 +1,77 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  name: "chart",
+  data() {
+    return {
+      data: {
+        labels: [
+          "10歳未満",
+          "10代",
+          "20代",
+          "30代",
+          "40代",
+          "50代",
+          "60代",
+          "70代",
+          "80代以上"
+        ],
+        datasets: [
+          {
+            label: "感染者数",
+            data: [10, 20, 30, 40, 50, 30, 60, 40, 50],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(10, 200, 64, 0.2)",
+              "rgba(55, 80, 200, 0.2)",
+              "rgba(122, 122, 122, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+              "rgba(10, 200, 64, 1)",
+              "rgba(55, 80, 200, 1)",
+              "rgba(122, 122, 122, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "年代"
+              }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                stepSize: 10
+              }
+            }
+          ]
+        }
+      }
+    };
+  },
+  mounted() {
+    this.renderChart(this.data, this.options);
+  }
+};
+</script>
--- src/views/Gender.vue
+++ src/views/Gender.vue
@@ -1,21 +1,25 @@
 <template>
   <v-col>
-    <v-card height="80vh" class="pa-3">
-      <p class="display-1">男女別感染者グラフを表示します</p>
-      <GenderGraph>
-        <doughnut-chart></doughnut-chart>
-      </GenderGraph>
-    </v-card>
+    <p class="display-1">男女別感染者グラフ</p>
+    <GenderGraph>
+      <doughnut-chart></doughnut-chart>
+    </GenderGraph>
+    <p class="display-1">年代別感染者グラフ</p>
+    <AgeGraph>
+      <bar-chart></bar-chart>
+    </AgeGraph>
   </v-col>
 </template>
 
 <script>
 import GenderGraph from "@/components/GenderGraph.vue";
+import AgeGraph from "@/components/AgeGraph.vue";
 
 export default {
   name: "Gender",
   components: {
-    GenderGraph
+    GenderGraph,
+    AgeGraph
   }
 };
 </script>
