33636c2ef79bfccb073fb68f62a024c6ee4ee026
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2020-04-08 23:59:18 +0900

    feat:死亡者数表示完了

--- src/views/Dead.vue
+++ src/views/Dead.vue
@@ -1,10 +1,25 @@
 <template>
-    <v-col>
-      <v-card height="80vh" class="pa-3">
-        <v-card-text>
-          <p class="display-1">This is an about page</p>
-          <p>日本国内における新型コロナウイルス感染症 (COVID-19)に関する情報サイトです。</p>
-        </v-card-text>
-      </v-card>
-    </v-col>
+  <v-col>
+    <v-card height="80vh" class="pa-3">
+      <v-card-text>
+        <p class="display-1">This is an about page</p>
+        <p>
+          日本国内における新型コロナウイルス感染症
+          (COVID-19)に関する情報サイトです。
+        </p>
+        <totalDead></totalDead>
+       
+      </v-card-text>
+    </v-card>
+  </v-col>
 </template>
+
+<script>
+import TotalDead from "@/components/TotalDead";
+export default {
+  components: {
+    TotalDead,
+  },
+};
+
+</script>
