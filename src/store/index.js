ef894930a6eb093d0dbc0922e91ca8e9c6424c12
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2020-04-09 15:10:51 +0900

    感染者グラフ完成

--- src/store/index.js
+++ src/store/index.js
@@ -185,7 +185,7 @@ export default new Vuex.Store({
     getDeadDeta(state){
       //配列の中から必要なdeadだけの配列を作成
       const deadarray = state.masterData.map(x=> x.dead )
-      console.log(Math.max.apply(null, deadarray))
+      
       return Math.max.apply(null, deadarray)
       
   
