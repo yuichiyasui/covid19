6923a3ac3dc17abce3dca521f4c230192ac3c789
yuichiyasui <yassan.b.a.w@gmail.com>
2020-04-16 13:53:21 +0900

    更新情報をAboutに追加

---
+++ src/components/about/UpdateInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card outlined>
+    <v-card-text class="mx-2 mt-2">
+      <div class="title title-color mb-5">{{ title }}</div>
+      <div v-for="(c, i) in contents" :key="i" class="mb-5">
+        <div class="font-weight-black">
+          {{ c.date }}
+        </div>
+        <div class="py-2" v-html="c.content"></div>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "更新情報",
+      contents: [
+        { date: "2020.04.07", content: "当サイトをオープンしました。" },
+        { date: "2020.04.16", content: "更新情報を表示するようにしました。" },
+        {
+          date: "2020.04.17",
+          content:
+            "年代別の感染者数の推移グラフを追加しました。<br>各都道府県別の感染者推移グラフを追加しました。"
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,10 @@
 <template>
   <v-col>
     <v-row justify="center" no-gutters align="center">
-      <v-col sm="8">
-        <p class="display-2 font-weight-thin orange--text text--darken-4">About</p>
+      <v-col sm="8" class="mb-10">
+        <p class="display-2 font-weight-thin orange--text text--darken-4">
+          About
+        </p>
         <v-card outlined>
           <v-card-text class="mx-2 mt-2">
             <Description class="mb-9" />
@@ -13,6 +15,14 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-row justify="center" no-gutters align="center">
+      <v-col sm="8" class="mb-10">
+        <p class="display-2 font-weight-thin orange--text text--darken-4">
+          Updates
+        </p>
+        <UpdateInfo />
+      </v-col>
+    </v-row>
   </v-col>
 </template>
 <script>
@@ -20,12 +30,14 @@ import DataSource from "@/components/about/DataSource";
 import Description from "@/components/about/Description";
 import Browser from "@/components/about/Browser";
 import JavaScript from "@/components/about/JavaScript";
+import UpdateInfo from "@/components/about/UpdateInfo";
 export default {
   components: {
     DataSource,
     Description,
     Browser,
-    JavaScript
+    JavaScript,
+    UpdateInfo
   }
 };
 </script>
