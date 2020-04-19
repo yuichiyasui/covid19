7a62471910ddd33aa6b81355b9961b47981f3cb9
shinyahashimoto <shinya.hashimoto@rakus-partners.co.jp>
2020-04-19 17:35:41 +0900

    退院数が0の日付も表示する

--- src/components/DischargeTransitionGraph.vue
+++ src/components/DischargeTransitionGraph.vue
@@ -6,84 +6,23 @@ export default {
   extends: LineChart,
   data() {
     return {
-      data: [],
+      data: []
     };
   },
   computed: {
     //日付の箱を取得
-    getDate: function(){
-      const dateArray = this.$store.getters.getDischargeTransition.map((item) =>
-        item.date.toLocaleDateString()
-      );
+    getDate: function() {
+      const dateArray = this.data.map(item => item.date.toLocaleDateString());
       return dateArray;
     },
-      //日別の退院数
-    getNumber: function () {
-      const dischargeArray = this.$store.getters.getDischargeTransition.map(
-        (item) => item.discharge
-      );
+    //日別の退院数
+    getNumber: function() {
+      const dischargeArray = this.data.map(item => item.todayDischarge);
       return dischargeArray;
-    },
-    // countDischargeNum: function() {
-    //   //storeからcsvデータを取得
-    //   var masterDate = this.$store.state.masterData;
-    //   // 日別毎の集計をするためのMapを宣言
-    //   var dischargeOfDayMap = new Map();
-    //   // 各レコードを保持する容器を宣言
-    //   var entities = [];
-    //   for (let i = 0; i < masterDate.length; i++) {
-    //     // 各レコードを取得
-    //     entities = masterDate[i];
-    //     var result = JSON.stringify(entities);
-    //     var results = JSON.parse(result);
-    //     // レコードから確定日を取得
-    //     var confirmDay = results.date;
-    //     // レコードからステータスを取得
-    //     var discharge = results.discharge;
-    //     if (discharge && discharge != 0) {
-    //       console.log(discharge);
-    //       console.log(confirmDay);
-    //       this.dischargeOfDayMap.set(confirmDay, discharge);
-    //     }
-    //   }
-    //   return dischargeOfDayMap;
-    // },
-    // getConfirmDate: function() {
-    //   var dischargeConfirmDate = [];
-    //   var entities = [];
-    //   for (let i = 0; i < this.data.length; i++) {
-    //     // 各レコードを取得
-    //     entities = this.data[i];
-    //     // レコードから確定日を取得
-    //     // レコードからステータスを取得
-    //     var confirmDay = entities.date;
-    //     var discharge = entities.discharge;
-    //     if (discharge && discharge != 0) {
-    //       dischargeConfirmDate.push(
-    //         this.$store.getters.dateToString(confirmDay)
-    //       );
-    //     }
-    //   }
-    //   return dischargeConfirmDate;
-    // },
-    // getNumber: function() {
-    //   var dischargeNumber = [];
-    //   var entities = [];
-    //   for (let i = 0; i < this.data.length; i++) {
-    //     // 各レコードを取得
-    //     entities = this.data[i];
-    //     // レコードから確定日を取得
-    //     // レコードからステータスを取得
-    //     var discharge = entities.discharge;
-    //     if (discharge && discharge != 0) {
-    //       dischargeNumber.push(discharge);
-    //     }
-    //   }
-    //   return dischargeNumber;
-    // },
+    }
   },
   mounted() {
-    this.date = this.$store.getters.getDischargeTransition;
+    this.data = this.$store.getters.getDischargeDates;
     this.renderChart(
       {
         labels: this.getDate,
@@ -93,12 +32,12 @@ export default {
             data: this.getNumber,
             borderColor: "#f57c00",
             backgroundColor: "rgba(255, 130, 3, 0.2)",
-            radius: 3,
-          },
-        ],
+            radius: 3
+          }
+        ]
       },
       { responsive: true, display: true, maintainAspectRatio: false }
     );
-  },
+  }
 };
 </script>
