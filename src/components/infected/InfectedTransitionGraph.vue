7e530ef0c6a1a7accc1ffa3336d80f1536edceb4
yuichiyasui <yassan.b.a.w@gmail.com>
2020-04-22 16:15:39 +0900

    感染者の累計、日別の切り替え実装

--- src/components/infected/InfectedTransitionGraph.vue
+++ src/components/infected/InfectedTransitionGraph.vue
@@ -1,52 +1,19 @@
 <script>
-import LineChart from "@/assets/js/LineChart.js";
+import { Line, mixins } from "vue-chartjs";
 
 export default {
   name: "TransitionGraph",
-  extends: LineChart,
-  data() {
-    return {
-      date: [],
-      dayBytotal: []
-    };
-  },
-  computed: {
-    //日付
-    getDate: function() {
-      var dateArray = [];
-      this.date.forEach((element) => {
-        dateArray.push(this.$store.getters.dateToString(element.date));
-      });
-      return dateArray;
-    },
-    //日別の感染者数
-    getCount: function() {
-      var countArray = [];
-      this.date.forEach((element) => {
-        countArray.push(element.count);
-      });
-      return countArray;
-    },
+  extends: Line,
+  mixins: [mixins.reactiveData],
+  props: ["infectedChartData", "options", "selected"],
+  watch:{
+    selected:function(){
+      this.renderChart(this.infectedChartData, this.options);
+    }
   },
   mounted() {
-    this.date = this.$store.getters.getInfectedTransition;
-    this.dayBytotal = this.$store.getters.getInfectedByDayTotal;
-    this.renderChart(
-      {
-        labels: this.getDate,
-        datasets: [
-          {
-            label: "感染者数",
-            data: this.getCount,
-            borderColor: "#f57c00",
-            backgroundColor: "rgba(255, 130, 3, 0.2)",
-            radius: 3,
-          },
-        ],
-      },
-      { responsive: true, display: true, maintainAspectRatio: false }
-    );
+    this.renderChart(this.infectedChartData, this.options);
   },
-  methods: {},
+  methods: {}
 };
 </script>
--- src/components/infected/InfectedTransitionGraphCard.vue
+++ src/components/infected/InfectedTransitionGraphCard.vue
@@ -2,9 +2,26 @@
   <v-card outlined class="pa-3 mb-4">
     <v-card-text class="pa-0">
       <v-card-title class="title-color">感染者推移</v-card-title>
-      <InfectedTransitionGraph>
-        <date-person-chart></date-person-chart>
-      </InfectedTransitionGraph>
+      <v-row align="center" justify="center">
+        <v-btn-toggle
+          v-model="toggle_exclusive"
+          mandatory
+          class="ml-auto mr-5"
+          dense
+        >
+          <v-btn :disabled="byDayButton" @click="switchGraph()">
+            日別
+          </v-btn>
+          <v-btn :disabled="totalButton" @click="switchGraph()">
+            累計
+          </v-btn>
+        </v-btn-toggle>
+      </v-row>
+      <InfectedTransitionGraph
+        :infected-chart-data="chartData"
+        :options="options"
+        :selected="selected"
+      />
     </v-card-text>
   </v-card>
 </template>
@@ -13,6 +30,62 @@ import InfectedTransitionGraph from "@/components/infected/InfectedTransitionGra
 export default {
   components: {
     InfectedTransitionGraph
+  },
+  data() {
+    return {
+      toggle_exclusive: undefined,
+      byDayButton: true, // ボタンのdisabled
+      totalButton: false, // ボタンのdisabled
+      selected: "日別",
+      chartData: {
+        labels: ["4/1", "4/2", "4/3"],
+        datasets: [
+          {
+            label: "感染者数",
+            data: ["100", "300", "500"],
+            borderColor: "#f57c00",
+            backgroundColor: "rgba(255, 130, 3, 0.2)",
+            radius: 3
+          }
+        ]
+      },
+      options: { responsive: true, display: true, maintainAspectRatio: false }
+    };
+  },
+  computed: {
+    setInfectedDataByTotal() {
+      return this.$store.getters.getInfectedByDayTotal;
+    },
+    setInfectedDataByDay() {
+      return this.$store.getters.getInfectedTransition;
+    }
+  },
+  methods: {
+    setChartData(infectedDataArray) {
+      this.chartData.labels = infectedDataArray.map(elm =>
+        this.$store.getters.dateToString(elm.date)
+      );
+      this.chartData.datasets[0].data = infectedDataArray.map(elm => elm.count);
+    },
+    switchGraph() {
+      if (this.byDayButton === true) {
+        // 累計ボタンが押された時
+        this.setChartData(this.setInfectedDataByTotal);
+        this.selected = "累計";
+        this.byDayButton = false;
+        this.totalButton = true;
+      } else {
+        // 日別ボタンが押された時
+        this.setChartData(this.setInfectedDataByDay);
+        this.selected = "日別";
+        this.byDayButton = true;
+        this.totalButton = false;
+      }
+    }
+  },
+  created() {
+    var infectedDataArray = this.setInfectedDataByDay;
+    this.setChartData(infectedDataArray);
   }
 };
 </script>
